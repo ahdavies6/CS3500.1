894fb9b1e91cc1e274f5394e945f42be1424484c
Adam Davies <ahdavies6@gmail.com>
2018-02-15 17:34:09 -0700

    added spreadsheet class, todo comments, and reworked solution file

---
+++ Spreadsheet/Spreadsheet/Spreadsheet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SS;
+
+namespace Spreadsheet
+{
+    class Spreadsheet : AbstractSpreadsheet
+    {
+        // todo: remove all comments (including todos)
+        // todo: implement all abstract methods
+        // todo: implement zero-argument constructor
+        // todo: make sure the next line works in another namespace
+        //AbstractSpreadsheet sheet = new Spreadsheet();
+    }
+}
